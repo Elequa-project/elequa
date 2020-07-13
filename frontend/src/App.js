aac451db20dc5facedce74dff040ac60276ee906
James Muralles <james.muralles23@gmail.com>
2020-07-13 18:57:53 -0500

    adding authorization security

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
 
 import React, { Component } from "react";
-import AddEmployee from "./Components/AddEmployee";
+// import AddEmployee from "./Components/AddEmployee";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import HelloWorld from "./components/HelloWorld";
+import Login from   "./components/login/Login"
+import Register from   "./components/registration/Register"
 class App extends Component {
     render() {
         return (
             <Router>
                 <Route exact path="/" component={HelloWorld} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+
             </Router>
         );
     }
--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -3,65 +3,91 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-import axios from 'axios';
+// import axios from 'axios';
+import AuthenticationService from "../../AuthenticationService";
 
-class Login extends React.Component{
+class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state= {
-            username: "",
-            password: ""
-        }
+
+        this.state = {
+            username: 'in28minutes',
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
+        };
+        // this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        // this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+        this.loginClicked = this.loginClicked.bind(this);
     }
-    render(){
+
+    handleChange(event) {
+        //console.log(this.state);
+        this.setState(
+            {
+                [event.target.name]: event.target.value
+            }
+        )
+    }
+
+    loginClicked() {
+        AuthenticationService
+            .executeJwtAuthenticationService(this.state.username, this.state.password)
+            .then((response) => {
+                AuthenticationService.registerSuccessfulLoginForJwt(this.state.username, response.data.token)
+                this.props.history.push(`/welcome/${this.state.username}`)
+            }).catch(() => {
+            this.setState({showSuccessMessage: false});
+            this.setState({hasLoginFailed: true});
+        })
+
+    }
+
+
+    render() {
         return (
+            //     <div>
+            //         <MuiThemeProvider>
+            //             <div>
+            //                 <AppBar title="login">
+            //                 </AppBar>
+            //                 <TextField hintText="Enter your username" floatingLabelText="Username"
+            //                            onChange={(event, newValue) => this.setState({username: newValue})}>
+            //                 </TextField>
+            //                 <br/>
+            //                 <TextField hintText="Enter your password" floatingLabelText="Password"
+            //                            onChange={(event, newValue) => this.setState({password: newValue})}>
+            //                 </TextField>
+            //                 <br/>
+            //                 <RaisedButton label="Submit" primary={true} style={style}
+            //                               onClick={(event) => this.handleClick(event)}>
+            //                 </RaisedButton>
+            //             </div>
+            //         </MuiThemeProvider>
+            //     </div>
+            // )
             <div>
-                <MuiThemeProvider>
-                    <div>
-                        <AppBar title="login">
-                        </AppBar>
-                        <TextField hintText="Enter your username" floatingLabelText="Username" onChange={(event, newValue)=>this.setState({username:newValue})}>
-                        </TextField>
-                        <br/>
-                        <TextField hintText="Enter your password" floatingLabelText="Password" onChange={(event, newValue)=>this.setState({password:newValue})}>
-                        </TextField>
-                        <br/>
-                        <RaisedButton label="Submit" primary={true} style={style} onClick={(event)=>this.handleClick(event)}>
-                        </RaisedButton>
-                    </div>
+                <MuiThemeProvider className="container">
+                <AppBar>Login</AppBar>
+                    {/*<ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>*/}
+                    {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
+                    {this.state.showSuccessMessage && <div>Login Sucessful</div>}
+                    {/*<ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>*/}
+                   <TextField type="text" name="username" hintText="Enter your username" floatingLabelText="Username"  value={this.state.username}
+                                      onChange={this.handleChange}/>
+                    <br/>
+                    <TextField type="password" name="password" hintText="Enter your password" floatingLabelText="Password"  value={this.state.password}
+                               onChange={this.handleChange}/>
+                    <br/>
+                    <RaisedButton label="Submit" primary={true} style={style} onClick={this.loginClicked}/>
                 </MuiThemeProvider>
             </div>
         )
-    }
-    handleClick(event){
-        var apiBasedUrl = "http://localhost:8080/api/";
-        var self = this;
-        var payload = {
-            "email":this.state.username,
-            "password":this.state.password
-        };
-
-        axios.post(apiBasedUrl + 'login', payload)
-            .then(function (response) {
-                console.log(response);
-                if(response.data.code === 200){
-                    console.log("login successful");
-                    var uploadScreen = [];
-                    uploadScreen.push(<uploadScreen appContext = {self.props.appContext}/>);
-                    self.props.appContext.setState({loginPage:[], uploadScreen:uploadScreen})
-                }else if(response.data.code === 204){
-                    console.log("username password do not match");
-                }else{
-                    console.log("username does not exist");
-                    alert("Username does not exist");
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
     }
 }
 const style = {
     margin: 15,
 };
+
 export default Login;
