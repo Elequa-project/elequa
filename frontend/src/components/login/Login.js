4b2960492d1e7bebca8e5aef8081139b4f7a629f
James Muralles <james.muralles23@gmail.com>
2020-07-16 17:32:45 -0500

    changed the Login.js

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,94 +1,134 @@
 import React from "react";
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 // import axios from 'axios';
 import AuthenticationService from "../../AuthenticationService";
+import { Container, Button } from "@material-ui/core";
+import AuthService from "../../services/auth.service";
+import Form from "react-validation/build/form";
+import CheckButton from "react-validation/build/button";
+
+
 
 class Login extends React.Component {
+
     constructor(props) {
         super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
 
         this.state = {
-            username: 'in28minutes',
-            password: '',
-            hasLoginFailed: false,
-            showSuccessMessage: false
+            username: "",
+            password: "",
+            loading: false,
+            message: ""
         };
-        // this.handleUsernameChange = this.handleUsernameChange.bind(this)
-        // this.handlePasswordChange = this.handlePasswordChange.bind(this)
-        this.handleChange = this.handleChange.bind(this);
-        this.loginClicked = this.loginClicked.bind(this);
     }
 
-    handleChange(event) {
-        //console.log(this.state);
-        this.setState(
-            {
-                [event.target.name]: event.target.value
-            }
-        )
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
     }
 
-    loginClicked() {
-        AuthenticationService
-            .executeJwtAuthenticationService(this.state.username, this.state.password)
-            .then((response) => {
-                AuthenticationService.registerSuccessfulLoginForJwt(this.state.username, response.data.token)
-                this.props.history.push(`/submissions/${this.state.username}`)
-            }).catch(() => {
-            this.setState({showSuccessMessage: false});
-            this.setState({hasLoginFailed: true});
-            this.setState({password: ""});
-
-        })
-
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        });
     }
 
+    handleLogin(e) {
+        e.preventDefault();
+
+        this.setState({
+            message: "",
+            loading: true
+        });
+
+        this.form.validateAll();
+
+        if (this.checkBtn.context._errors.length === 0) {
+            AuthService.login(this.state.username, this.state.password).then(
+                () => {
+                    this.props.history.push("/profile");
+                    window.location.reload();
+                },
+                error => {
+                    const resMessage =
+                        (error.response &&
+                            error.response.data &&
+                            error.response.data.message) ||
+                        error.message ||
+                        error.toString();
+
+                    this.setState({
+                        loading: false,
+                        message: resMessage
+                    });
+                }
+            );
+        } else {
+            this.setState({
+                loading: false
+            });
+        }
+    }
 
     render() {
         return (
-            //     <div>
-            //         <MuiThemeProvider>
-            //             <div>
-            //                 <AppBar title="login">
-            //                 </AppBar>
-            //                 <TextField hintText="Enter your username" floatingLabelText="Username"
-            //                            onChange={(event, newValue) => this.setState({username: newValue})}>
-            //                 </TextField>
-            //                 <br/>
-            //                 <TextField hintText="Enter your password" floatingLabelText="Password"
-            //                            onChange={(event, newValue) => this.setState({password: newValue})}>
-            //                 </TextField>
-            //                 <br/>
-            //                 <RaisedButton label="Submit" primary={true} style={style}
-            //                               onClick={(event) => this.handleClick(event)}>
-            //                 </RaisedButton>
-            //             </div>
-            //         </MuiThemeProvider>
-            //     </div>
-            // )
-            <div  >
-                <MuiThemeProvider >
+                <MuiThemeProvider>
                 <AppBar>Login</AppBar>
+            <Container>
                     {/*<ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>*/}
                     {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
                     {this.state.showSuccessMessage && <div>Login Sucessful</div>}
                     {/*<ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>*/}
-                   <div style={style}>
+                   <Form onSubmit={this.handleLogin}
+                         ref={c => {
+                             this.form = c;
+                         }}
+                         validate>
+                    <div style={style}>
 
-                    <TextField  type="text" name="username" hintText="Enter your username" floatingLabelText="Username"  value={this.state.username}
-                                      onChange={this.handleChange}/>
+                    <TextField  type="text"
+                                name="username"
+                                hintText="Enter your username"
+                                floatingLabelText="Username"
+                                value={this.state.username}
+                                onChange={this.onChangeUsername} required/>
                     <br/>
-                    <TextField type="password" name="password" hintText="Enter your password" floatingLabelText="Password"  value={this.state.password}
-                               onChange={this.handleChange}/>
+                    <TextField type="password"
+                               name="password"
+                               hintText="Enter your password"
+                               floatingLabelText="Password"
+                               value={this.state.password}
+                               onChange={this.onChangePassword} required/>
                                <br/>
-                    <RaisedButton label="Submit" primary={true}  onClick={this.loginClicked}/>
+                    <RaisedButton label="Submit" primary={true} variant="contained" type="submit" />
                     <br/>
+
+                       {this.state.message && (
+                           <div className="form-group">
+                               <div className="alert alert-danger" role="alert">
+                                   {this.state.message}
+                               </div>
+                           </div>
+                       )}
+
+                       <CheckButton
+                           style={{ display: "none" }}
+                           ref={c => {
+                               this.checkBtn = c;
+                           }}
+                       />
                    </div>
+                       </Form>
+            </Container>
                 </MuiThemeProvider>
-            </div>
         )
     }
 }
